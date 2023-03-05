caa12a33d48f4ba6aceedd13850d3795c7e011d6
Pavel Buyanov <pacifist277@mail.ru>
2023-03-05 20:58:29 +0400

    Add collapsing toolbar  for portrait . But it works through ass.

--- app/src/main/java/com/ps_bn/visitcard/Activities/NewsDetailsActivity.java
+++ app/src/main/java/com/ps_bn/visitcard/Activities/NewsDetailsActivity.java
@@ -16,25 +16,31 @@ public class NewsDetailsActivity extends AppCompatActivity {
     private TextView title;
     private TextView fullText;
     private TextView date;
+    private TextView toolbarTitle;
     private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        assert actionBar != null;
+        actionBar.hide();
         setContentView(R.layout.activity_news_details);
+        toolbarTitle = findViewById(R.id.toolbar_title);
         image = findViewById(R.id.details_full_image);
         title = findViewById(R.id.title_detail_text);
         fullText = findViewById(R.id.full_detail_text);
         date = findViewById(R.id.date_detail_text);
-        ActionBar actionBar=getSupportActionBar();
         Intent intent = getIntent();
         if (intent != null) {
             title.setText(intent.getStringExtra("title"));
             fullText.setText(intent.getStringExtra("fulltext"));
             date.setText(intent.getStringExtra("date"));
-           actionBar.setTitle(intent.getStringExtra("category"));
+            toolbarTitle.setText(intent.getStringExtra("category"));
 
-            DownloadTask task = new DownloadTask(image,image);
+            DownloadTask task = new DownloadTask(image, image);
             task.getDownloadImage().execute(intent.getStringExtra("image_url"));
         }
     }
